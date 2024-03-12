7b0c5e29bfb6e10e4cdf6e09c91c87d94ef422e4
GhostDeQueso <mateuszryngiewicz1@gmail.com>
2024-03-12 21:23:18 +0100

    3 func

---
+++ stadium.py
@@ -0,0 +1,46 @@
+def main():
+
+    cat_a = 20
+    cat_b = 15
+    cat_c = 10
+    cat_a - int(cat_a)
+    cat_b - int(cat_b)
+    cat_c - int(cat_c)
+
+
+    amount_a = int(input("how many A: "))
+    amount_b = int(input("how many B: "))
+    amount_c = int(input("how many C: "))
+
+
+    total = ticketA(amount_a) + ticketB(amount_b) + ticketC(amount_c)
+    print(total)
+
+def ticketA(amount_a):
+
+    ticket = amount_a * 20
+    
+    return ticket
+
+def ticketB(amount_b):
+
+    ticket = amount_b * 15
+    
+    return ticket
+
+def ticketC(amount_c):
+
+    ticket = amount_c * 10
+    
+    return ticket
+
+
+
+
+
+
+
+
+
+main()
+    
